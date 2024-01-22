0c257c1a079fda3ba0f8d06001ccfa96095a14c3
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-01-22 16:12:46 -0300

    style: remove stitches on card

--- src/container/home.tsx
+++ src/container/home.tsx
@@ -1,31 +1,20 @@
 import { useState, useEffect, useTransition } from 'react'
 import { Icon } from '../assets/svg.access';
 import { getPayload } from '../service/service.token'
-import { Card, CardContainer } from './template/card'
-import { Header, TitleHeader } from './template/header'
 import { vector } from './menu';
 import { UriToScreenFormat } from '../assets/uri.format'
 import { accessList } from './access.list'
 import { Button } from './template/button/button'
+import { Header } from './template/header/header';
+import { Cards } from './template/card/card';
 
 export const Home = () => {
-    // const [ispending, startTransition] = useTransition()
     const [list, setList] = useState<boolean[]>(accessList())
 
-    // useEffect(()=> {
-    //     startTransition(() => setList(accessList()))
-    // },[])
     return (
         <>
-            <Header>
-                <TitleHeader><h1>Home</h1></TitleHeader>
-                <a href={`#/${'profile'}`}><Button category={'secondary'} name={getPayload().sub}/></a>
-            </Header>
-            <CardContainer>
-                {vector.map((element, index) => {
-                    return <Card key={Math.random()}><a key={Math.random()} href={`#/${vector[index][0]}`}><Icon name={vector[index][1]} /><p>{UriToScreenFormat(vector[index][0])}</p></a></Card>
-                })}
-            </CardContainer>
+            <Header title='Home'/>
+            <Cards></Cards>
         </>
     );
 }
