ee6a40748a2adf68cbe2c00e79e66403a6e15a30
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-01-23 18:03:17 -0300

    style: remove stitches on rotate

--- src/container/page/login/login.tsx
+++ src/container/page/login/login.tsx
@@ -10,7 +10,6 @@ import { Button } from '../../template/button/button';
 import { logout } from '../../../service/service.crud'
 import { existsToken, getPayload, isValidToken } from '../../../service/service.token'
 import logo from '../../../assets/image/coffee2.png'
-import { Rotate } from '../../template/rotate'
 import { Toast } from '../../template/toast/toast.style'
 import { Home } from '../home'
 import './login.css'
