766d2d070ace9f8aff584ef13d27e39d85c26e10
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-01-23 09:22:26 -0300

    style: define classname

--- src/container/template/card/card.tsx
+++ src/container/template/card/card.tsx
@@ -5,7 +5,7 @@ import './card.css'
 
 export const Cards = () => {
     return (
-        <div>
+        <div className='card'>
             {vector.map((element, index) => {
                 return <span key={Math.random()}><a key={Math.random()} href={`#/${vector[index][0]}`}><Icon name={vector[index][1]} /><p>{UriToScreenFormat(vector[index][0])}</p></a></span>
             })}
--- src/container/template/header/header.tsx
+++ src/container/template/header/header.tsx
@@ -12,7 +12,7 @@ interface IHeader {
 
 export const Header = (header: IHeader) => {
     return (
-        <header>
+        <header className='header'>
             {vector.find((element)=> element.includes(header.title)) && <Button category={'primary'} function={header.function} name={'New'}/>}
             <h1>{UriToScreenFormat(header.title)}</h1>
             <a href={`#/${'profile'}`}><Button category={'secondary'} name={getPayload().sub} /></a>
