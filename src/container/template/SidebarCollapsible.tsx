56461ba21d190c99e5de0c23594631678a135215
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-01-23 18:03:32 -0300

    chore: change file name

--- src/container/template/SidebarCollapsible.tsx
+++ src/container/template/SidebarCollapsible.tsx
@@ -3,45 +3,6 @@ import logo from '../../assets/image/marinha.png'
 
 const { styled } = stitches
 
-export const CenterContainer = styled('div', {
-    backgroundColor: '$second',
-    backgroundImage: 'linear-gradient(to bottom, $second, $fourth)',
-    minHeight: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-})
-export const CenterItem = styled('div', {
-    color: '$fourteenth',
-    padding: '5rem 0rem 5rem 0',
-    borderRadius: '.3rem',
-    width: '18rem',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    'img': {
-        margin: '2em',
-    },
-    'span': {
-        height: '1em',
-    },
-    'button': {
-        margin: '2em',
-    },
-    variants: {
-        direction: {
-            column: {
-                flexDirection: 'column',
-            },
-            row: {
-                minHeight: '0',
-                flexDirection: 'row',
-                justifyContent: 'center',
-            },
-        },
-    },
-})
-
 export const SidebarCollapsible = styled('div',{
     display: 'flex',
     justifyContent: 'space-between',
