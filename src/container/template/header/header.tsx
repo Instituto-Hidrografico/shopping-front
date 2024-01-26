d8dd3283be315343ed514c246354f2b02807fdc8
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-01-26 16:11:17 -0300

    style: input group css

--- src/container/template/header/header.tsx
+++ src/container/template/header/header.tsx
@@ -4,6 +4,8 @@ import { getPayload } from '../../../service/service.token'
 import { Button } from '../button/button'
 import { vector } from '../../menu/category'
 import './header.css'
+import { InputGroup } from '../../page/generic.field'
+import './../inputgroup/inputgroup.css'
 
 interface IHeader {
     title: string,
@@ -15,6 +17,15 @@ export const Header = (header: IHeader) => {
         <header className='header'>
             {vector.find((element)=> element.includes(header.title)) && <Button category={'primary'} function={header.function} name={'New'}/>}
             <h1>{UriToScreenFormat(header.title)}</h1>
+
+
+            <span className='.inputgroup'>
+                <input disabled={false} type="text" name={"ddddddd"} placeholder={'ddddddd'} />
+                <label htmlFor={"ddddddd"}>{}</label>
+                <span>{"ddddddd"}</span>
+            </span>
+
+
             <a href={`#/${'profile'}`}><Button category={'secondary'} name={getPayload().sub} /></a>
         </header>
     )
