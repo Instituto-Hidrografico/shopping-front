c69aa0e7c1249971f6d034279ecd60da16be5a7f
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-01-29 13:37:44 -0300

    style: add input group

--- src/container/template/header/header.tsx
+++ src/container/template/header/header.tsx
@@ -19,11 +19,13 @@ export const Header = (header: IHeader) => {
             <h1>{UriToScreenFormat(header.title)}</h1>
 
 
-            <span className='.inputgroup'>
-                <input disabled={false} type="text" name={"ddddddd"} placeholder={'ddddddd'} />
-                <label htmlFor={"ddddddd"}>{}</label>
-                <span>{"ddddddd"}</span>
-            </span>
+            {/* <span className='inputgroup tooltip' data-tip={['']}>
+                <input disabled={true} className="title" value="4" tabIndex={0} />
+                <input disabled={true} type="text" name={"ddddddd"} placeholder={'ddddddd'} />
+                <label htmlFor={"ddddddd"} className={'validation'}>{"ddddddd"}</label>
+                <input disabled={false} type="text" name={"123"} placeholder={'123'} />
+                <label htmlFor={"123"} className={'validation'}>{"123"}</label>
+            </span> */}
 
 
             <a href={`#/${'profile'}`}><Button category={'secondary'} name={getPayload().sub} /></a>
