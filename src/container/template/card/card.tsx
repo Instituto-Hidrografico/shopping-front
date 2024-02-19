bdfb13ab26864e38f31e74ef10ac39d1d3322583
gadelhati <gadelhati@gmail.com>
2024-02-19 08:48:30 -0300

    bug: fix error

--- src/container/template/card/card.tsx
+++ src/container/template/card/card.tsx
@@ -21,12 +21,10 @@ export const Cards = () => {
         )
     }
     return (
-        <>
         <div className='card'>
             {vector.map((element, index) => {
                 return <span key={Math.random()}><a key={Math.random()} href={`#/${vector[index][0]}`}><Icon name={vector[index][1]} /><p>{UriToScreenFormat(vector[index][0])}{retrieveItem(vector[index][0])}</p></a></span>
             })}
         </div>
-        </>
     )
 }
